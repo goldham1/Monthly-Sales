ce2f0a7a0c38bf71fa0be402e84899262290922a
goldham1 <goldham1@mail.pima.edu>
2024-10-09 20:39:00 -0700

    Create CIS_GabeOldham_Lab4.py

---
+++ CIS_GabeOldham_Lab4.py
@@ -0,0 +1,52 @@
+# Module 4 Lab-4
+# Gabe Oldham
+# 10/08/2024
+# This program gets the monthly sales and sales increase from user then calculates
+# and displays the employee bonus and store bonus.
+
+
+
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount  = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+
+# This code gets the monthly sales
+prompt = "Please enter the monthly sales:" # prompt will be a string literal
+monthlySales = float(input(prompt))
+
+# This code gets the percent of increase in sales
+prompt = "Please enter the sales increase:" # prompt will be a string literal
+salesIncrease = float(input(prompt))
+salesIncrease = salesIncrease / 100
+
+# This code determines the storeAmount bonus
+
+if monthlySales > 110000:
+    storeAmount = 6000
+elif monthlySales >= 100000:
+    storeAmount = 5000
+elif monthlySales >= 90000:
+    storeAmount = 4000
+elif monthlySales >= 80000:
+    storeAmount = 3000
+else:
+    storeAmount = 0
+
+# This code determines the empAmount bonus
+if salesIncrease >= .05:
+    empAmount = 75
+elif salesIncrease >= .04:
+    empAmount = 50
+elif salesIncrease >= .03:
+    empAmount = 40
+else:
+    empAmount = 0
+
+# This code prints the bonus information
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+if (storeAmount == 6000 ) and (empAmount == 75):
+    print('Congrats! You have reached the highest bonus amounts possible! ')
+
